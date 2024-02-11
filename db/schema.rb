217f7a6178b7dc7a7f0b02fe82aa0536b6eb5532
bamnelkarvivek <bamnelkarvivek@gmail.com>
2024-02-11 16:24:56 -0500

    Ruby on rails friends app

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_201108) do
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
