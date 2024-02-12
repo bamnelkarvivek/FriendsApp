f39d7f5f8fccb308085d933dc94616d9cc9fa1cd
bamnelkarvivek <bamnelkarvivek@gmail.com>
2024-02-12 16:18:01 -0500

    Added user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_005238) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_12_210050) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_005238) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
